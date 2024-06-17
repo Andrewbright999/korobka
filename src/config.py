99a969a149ff79fc49e8378f2beebada27248ae2
Andrew Bright <legendarkakr@gmail.com>
2024-06-17 18:47:46 +0300

    .env directory fixed

--- src/config.py
+++ src/config.py
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
     def DATABASE_URL_asyncpg(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file="../.env")
     
 settings = Settings()
 
