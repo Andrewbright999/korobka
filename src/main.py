8881b08f9da791d92898ed5797e145c787a5093e
Andrew Bright <legendarkakr@gmail.com>
2024-06-19 18:05:57 +0300

    404 Page and exception handler added

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import asyncio
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.templating import Jinja2Templates
+
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -10,11 +12,12 @@ from auth.routers import router as auth_router
 from pages.router import router as page_router
 from auth.admin import create_admin
 
+templates = Jinja2Templates(directory="../templates")
+
 
 app = FastAPI(
     title="Коробка"
 )
-
 
 app.include_router(auth_router)
 app.include_router(box_router)
@@ -23,27 +26,13 @@ app.include_router(page_router)
 
 app.mount("/static", StaticFiles(directory="../static"), name="static")
 
-
-# origins = [
-#     # "http://localhost:3000",
-# ]
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_credentials=True,
-#     allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
-#     allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-#                    "Authorization"],
-# )
-
-
-# @app.middleware("http")
-# async def auth_middleware(request, call_next):
-#     response = await call_next(request)
-#     if response.status_code == 401:
-#         return RedirectResponse(url="/login", status_code=302)
-
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
 
 @app.on_event("startup")
 async def startup_event():
@@ -53,6 +42,11 @@ async def startup_event():
         # await conn.run_sync(Base.metadata.create_all)
     await create_admin()
 
+
+@app.exception_handler(404)
+async def not_found_exception_handler(request: Request, exc: HTTPException):
+    context = {"request": request}
+    return templates.TemplateResponse("404page.html", context=context)
 
 @app.get("/")
 async def login_page():
@@ -65,6 +59,6 @@ async def login_page():
 #                         proxy_set_header X-Forwarded-Proto $scheme;
 #                         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for; # передаем всю последовательность адресов, через которые прошел запрос
 #                 }
-    
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
--- src/pages/router.py
+++ src/pages/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter(
@@ -10,12 +10,15 @@ templates = Jinja2Templates(directory="../templates")
 
 @router.get("/storage")
 def get_base_page(request: Request):
-    return templates.TemplateResponse("table.html", {"request": request})
+    return templates.TemplateResponse("storage.html", {"request": request})
 
 @router.get("/login")
 def get_base_page(request: Request):
     context = {"request": request}
-    return templates.TemplateResponse("index.html", context=context)
+    return templates.TemplateResponse("index.html", {"request": request})
+    
+    
+
 
 # @router.get("/test-login")
 # def get_base_page(request: Request):
