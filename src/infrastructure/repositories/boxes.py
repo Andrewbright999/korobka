110893bfb56a202de2de8d3d6b14c6d2337e1a13
Andrew Bright <legendarkakr@gmail.com>
2024-06-20 23:29:46 +0300

    Unusabla func deleted

--- src/infrastructure/repositories/boxes.py
+++ src/infrastructure/repositories/boxes.py
@@ -3,3 +3,6 @@ from domain.boxes.models import Box
 
 class BoxRepository(SQLAlchemyRepostitory):
     model = Box
+    
+    
+    
--- src/infrastructure/repositories/users.py
+++ src/infrastructure/repositories/users.py
@@ -4,3 +4,5 @@ from domain.users.models import User
 
 class UserRepository(SQLAlchemyRepostitory):
     model = User
+    
+    
